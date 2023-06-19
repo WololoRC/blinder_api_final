b36229797a86ae19b08ca02285be94e91a3d8127
WololoRC <wololoRC@proton.me>
2023-06-19 17:11:16 -0300

    sorry agustin :'(

--- api/urls.py
+++ api/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('tags.urls')),
-    path('api/', include('profiles.urls'))
+    path('api/', include('profiles.urls')),
+    path('api/', include('messages_app.urls'))
 ]
